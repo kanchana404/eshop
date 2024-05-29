56bf366069be0bd58e697b52217d05203b8b3a11
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-05-29 20:36:45 +0530

    photo awla haduwa

--- components/Hero.php
+++ components/Hero.php
@@ -19,37 +19,37 @@
         <div class="row text-center" style="margin-top:50px; ">
 
         <?php
-            $wpdall = Database::search("SELECT * FROM women INNER JOIN product ON product.id = women.product_id INNER JOIN product_img ON product_img.product_id = product.id INNER JOIN category ON product.category_c_id = category.c_id");
-            $wpdall_num = $wpdall->num_rows;
-            for ($x = 0; $x < $wpdall_num; $x++) {
-                $wpdall_data = $wpdall->fetch_assoc();
-                
-            ?>
-            
-            <div class="col-lg-3 col-md-6 col-sm-12 text-center" style=" margin: bottom 50px;">
-                <div class="container" id="box" >
-                    <img src="components/cloth_img/w-1.jpg" alt="" style=" clip-path: polygon(0 0, 100% 0, 100% 75%, 0 75%);">
-                    <div class="row text-center" style="margin-top:-80px;">
-                        <h5><?php echo $wpdall_data["title"] ?></h5>
-                    </div>
+$imgPaths = [];
+$wpdall = Database::search("SELECT * FROM women INNER JOIN product ON product.id = women.product_id INNER JOIN product_img ON product_img.product_id = product.id INNER JOIN category ON product.category_c_id = category.c_id");
+$wpdall_num = $wpdall->num_rows;
+for ($x = 0; $x < $wpdall_num; $x++) {
+    $wpdall_data = $wpdall->fetch_assoc();
+    $imgSrc = $wpdall_data["path"]; // Fetching the image path from the database
+    $imgPaths[] = $imgSrc;
+    ?>
+    
+    <div class="col-lg-3 col-md-6 col-sm-12 text-center" style=" margin: bottom 50px;">
+        <div class="container" id="box">
+            <img src="<?php echo $imgSrc; ?>" alt="" style="clip-path: polygon(0 0, 100% 0, 100% 75%, 0 75%);">
+            <div class="row text-center" style="margin-top:-80px;">
+                <h5><?php echo $wpdall_data["title"] ?></h5>
+            </div>
+            <div class="row">
+                <div class="col-6 text-start"><h6>Category:<?php echo $wpdall_data["c_name"] ?></h6></div>
+                <div class="col"></div>
+                <div class="col-5 text-end">
                     <div class="row">
-                        <div class="col-6 text-start"><h6>Category:<?php echo $wpdall_data["c_name"] ?></h6></div>
-                        
-                        <div class="col"></div>
-                        <div class="col-5 text-end">
-
-                        <div class="row">
-                                <div class="col text-center"> <i class="bi bi-bag"></i></div>
-                                <div class="col text-center"><i class="bi bi-heart"></i></div>
-                            </div>
-                        </div>
+                        <div class="col text-center"><i class="bi bi-bag"></i></div>
+                        <div class="col text-center"><i class="bi bi-heart"></i></div>
                     </div>
                 </div>
             </div>
-           
-            <?php
-            }
-            ?>
+        </div>
+    </div>
+    
+    <?php
+}
+?>
            
         </div>
         
